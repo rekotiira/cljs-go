85f6994283d26519a27836ce45a7289d2ccf10fa
Reko Tiira <reko@reaktor.fi>
2016-02-21 20:06:58 +0200

    refactor to keep all of app state in one global atom

--- src/cljs-go/core.cljs
+++ src/cljs-go/core.cljs
@@ -4,18 +4,11 @@
 
 (enable-console-print!)
 
-(defonce board-state (r/atom {:size 19
-                              :turn :black
-                              :stones {:black #{} :white #{}}}))
-
-(defonce board-history (r/atom '()))
-
-(add-watch board-state :board-history-watcher
-  (fn [key atom old-state new-state]
-    (swap! board-history conj new-state)))
-
-(defonce color (r/atom "empty"))
-(defonce liberties (r/atom 0))
+(defonce app-state
+  (r/atom {:board {:size 19
+                   :turn :black
+                   :stones {:black #{} :white #{}}}
+           :history '()}))
 
 (defn stone-as-string [stone]
   (case stone
@@ -70,65 +63,66 @@
   (let [neighbors (get-neighbors-for-stones board group)]
     (select (partial is-open-intersection? board) neighbors)))
 
-(defn find-adjacent-dead-stones [{turn :turn, stones :stones, :as board} pos]
-  (->> (stones (other-player turn))
+(defn find-adjacent-dead-stones [{stones :stones, :as board} player pos]
+  (->> (stones player)
        (get-neighbors board pos)
        (map (partial find-group-stones-at board))
        (filter #(empty? (find-group-liberties board %)))
        (apply concat)
        (set)))
 
-(defn remove-stones [board who stones]
-  (update-in board [:stones who] difference stones))
+(defn remove-stones [board player stones]
+  (update-in board [:stones player] difference stones))
 
-(defn valid-move? [{turn :turn, :as board} pos]
-  (and
-    (is-open-intersection? board pos)
+(defn place-stone [{turn :turn, :as board} history stone]
+  "Returns a new board after stone has been placed, or nil if invalid move"
+  (if (is-open-intersection? board stone)
     (let [opponent (other-player turn)
-          place-stone-step (update-in board [:stones turn] conj pos)
-          dead-stones (find-adjacent-dead-stones place-stone-step pos)
+          place-stone-step (update-in board [:stones turn] conj stone)
+          dead-stones (find-adjacent-dead-stones place-stone-step opponent stone)
           remove-dead-stones-step (remove-stones place-stone-step opponent dead-stones)
-          group (find-group-stones-at remove-dead-stones-step pos)
-          liberties (find-group-liberties remove-dead-stones-step group)
-          change-turn-step (assoc remove-dead-stones-step :turn opponent)]
-      (and
-        (seq liberties)
-        (not (= (second @board-history) change-turn-step))))))
+          new-board (assoc remove-dead-stones-step :turn opponent)
+          group (find-group-stones-at new-board stone)
+          liberties (find-group-liberties new-board group)]
+      (if (and
+            (seq liberties)
+            (not (= new-board (first history)))) ; check history
+        new-board))))
 
-(defn place-stone-and-change-turn [{turn :turn, :as board} stone]
-  (let [opponent (other-player turn)
-        place-stone-step (update-in board [:stones turn] conj stone)
-        dead-stones (find-adjacent-dead-stones place-stone-step stone)
-        remove-dead-stones-step (remove-stones place-stone-step opponent dead-stones)
-        change-turn-step (assoc remove-dead-stones-step :turn opponent)]
-    change-turn-step))
+(defn valid-move? [board history stone]
+  (not (nil? (place-stone board history stone))))
 
-(defn play-stone [{turn :turn, stones :stones, :as board} pos]
-  (if (valid-move? board pos)
-    (swap! board-state place-stone-and-change-turn pos)))
+(defn update-board-state [app-state old-board new-board]
+  (-> app-state
+    (assoc :board new-board)
+    (update :history conj old-board)))
 
-(defn update-debug-info [{stones :stones, :as board} valid-move-state pos]
-  (reset! color (stone-as-string (whose-stone board pos)))
-  (reset! liberties (count (find-group-liberties board (find-group-stones-at board pos)))))
+(defn play-stone [pos]
+  (let [{:keys [board history]} @app-state]
+    (if-let [new-board (place-stone board history pos)]
+      (swap! app-state update-board-state board new-board))))
 
-(defn intersection-component [board pos]
-  (let [valid-move-state (r/atom false)]
-    (fn [board [x y :as pos]]
-      (let [stone (whose-stone board pos)]
-        [:div {:class (str "intersection " (stone-as-string stone) (if @valid-move-state " valid-move"))
-               :on-click #(play-stone board pos)
-               :on-mouse-enter #(reset! valid-move-state (valid-move? board pos))
-               :on-mouse-leave #(reset! valid-move-state nil)}
-          (for [i ["top" "bottom"]
-                j ["left" "right"]]
-            ^{:key (str "subtile-" i "-" j)}
-            [:div {:class (str "subtile " i " " j)}])
-          [:div {:class "stone"}]]))))
+(defn valid-move-from-state? [pos]
+  (let [{:keys [board history]} @app-state]
+    (valid-move? board history pos)))
 
-(defn board-component [board-state]
-  (let [board @board-state
-        size (inc (:size board))
+(defn board-intersection [pos player]
+  (let [valid-move (r/atom nil)]
+    (fn [pos player]
+      [:div {:class (str "intersection " (stone-as-string player) (if @valid-move " valid-move"))
+             :on-click #(play-stone pos)
+             :on-mouse-enter #(reset! valid-move (valid-move-from-state? pos))
+             :on-mouse-leave #(reset! valid-move nil)}
+        (for [i ["top" "bottom"]
+              j ["left" "right"]]
+          ^{:key (str "subtile-" i "-" j)}
+          [:div {:class (str "subtile " i " " j)}])
+        [:div {:class "stone"}]])))
+
+(defn board []
+  (let [board (:board @app-state)
         turn (:turn board)
+        size (inc (:size board))
         stones (:stones board)]
     [:div {:class (str "game " (stone-as-string turn) "-turn")}
       [:div {:class "board"}
@@ -136,15 +130,14 @@
           ^{:key (str "row-" y)}
           [:div {:class "row"}
             (for [x (range size)]
-              ^{:key (str "intersection-" x "-" y)}
-              [intersection-component board [x y]])])]]))
+              (let [pos [x y]]
+                ^{:key (str "intersection-" x "-" y)}
+                [board-intersection pos (whose-stone board pos)]))])]]))
 
-(defn game-component []
-  [:div
-    [:div (str @color (if (> @liberties 0) (str ", liberties: " @liberties)))]
-    [board-component board-state]])
+(defn game []
+  [board])
 
-(r/render-component [game-component] (. js/document (getElementById "app")))
+(r/render-component [game] (. js/document (getElementById "app")))
 
 (defn on-js-reload []
   ;(swap! board update-in [:turn] #(if (= % "black") "white" "black"))
